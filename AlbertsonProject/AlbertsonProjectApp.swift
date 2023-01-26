28b1f32d6476be3988a25af5424aad8f2907b82b
Kingsley Enoka <kingsleyenoka@gmail.com>
2023-01-26 16:15:27 -0600

    Initial Commit

---
+++ AlbertsonProject/AlbertsonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertsonProjectApp.swift
+//  AlbertsonProject
+//
+//  Created by Kingsley Enoka on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AlbertsonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
